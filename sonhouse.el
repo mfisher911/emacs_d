c81fe626912a3d4a4448174f8107db87a77f995f
Mike Fisher <mfisher@sonhouse.son.rochester.edu>
2009-06-09 18:51:20 -0400

    Add contents for sonhouse.

---
+++ sonhouse.el
@@ -0,0 +1,9 @@
+(setq load-path (cons "/home/mfisher/emacs" load-path))
+(autoload 'js2-mode "js2" nil t)
+(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(js2-basic-offset 4))
