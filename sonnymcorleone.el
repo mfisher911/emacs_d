570745640bbe6154f794a63099a16c6d2bf87c82
Mike Fisher <mfisher@csh.rit.edu>
2009-11-12 18:24:53 -0500

    Added org mode stuff. Hope it works!

--- sonnymcorleone.el
+++ sonnymcorleone.el
@@ -21,3 +21,16 @@
 (windmove-default-keybindings 'shift)
 
 (server-mode 1)
+
+;;; Org Mode stuff
+(require 'org-install)
+;; The following lines are always needed.  Choose your own keys.
+(add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cb" 'org-iswitchb)
+(add-hook 'org-mode-hook 'turn-on-font-lock)  ; Org buffers only
+(setq org-mobile-directory "/Volumes/org")
+(setq org-attach-directory "/Volumes/org/data/")
+(setq org-directory "/Volumes/org")
+(setq org-publish-timestamp-directory "/Volumes/org/.org-timestamps/")
