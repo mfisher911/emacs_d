7c80570404b4df61be7e4d437e891c2ff5d829b7
Mike Fisher <mfisher@csh.rit.edu>
2009-11-13 09:56:49 -0500

    Org mode stuff.

--- lapblanket.el
+++ lapblanket.el
@@ -1,5 +1,76 @@
 ;; enable twitter.el (http://www.busydoingnothing.co.uk/twitter-el/)
-(autoload 'twitter-get-friends-timeline "twitter" nil t)
-(autoload 'twitter-status-edit "twitter" nil t)
-(global-set-key "\C-xt" 'twitter-get-friends-timeline)
-(add-hook 'twitter-status-edit-mode-hook 'longlines-mode)
+;; (autoload 'twitter-get-friends-timeline "twitter" nil t)
+;; (autoload 'twitter-status-edit "twitter" nil t)
+;; (global-set-key "\C-xw" 'twitter-get-friends-timeline)
+;; (add-hook 'twitter-status-edit-mode-hook 'longlines-mode)
+
+;;; yasnippet
+(require 'yasnippet-bundle)
+
+;;; http://doc.norang.ca/org-mode.html
+;;;
+;;; Org Mode
+;;;
+(require 'org-install)
+(add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cb" 'org-iswitchb)
+(add-hook 'org-mode-hook 'turn-on-font-lock)  ; Org buffers only
+;; Make TAB the yas trigger key in the org-mode-hook and turn on flyspell mode
+(add-hook 'org-mode-hook
+          (lambda ()
+            ;; yasnippet
+            (make-variable-buffer-local 'yas/trigger-key)
+            (setq yas/trigger-key [tab])
+            (define-key yas/keymap [tab] 'yas/next-field-group)
+            ;; flyspell mode to spell check everywhere
+            (flyspell-mode 1)))
+(setq org-agenda-files (quote ("/Volumes/org/work.org"
+                               "/Volumes/org/refile.org"
+                               )))
+(setq org-mobile-directory "/Volumes/org")
+(setq org-attach-directory "/Volumes/org/data/")
+(setq org-directory "/Volumes/org")
+(setq org-publish-timestamp-directory "/Volumes/org/.org-timestamps/")
+(setq org-default-notes-file "/Volumes/org/refile.org")
+
+;;;  Load Org Remember Stuff
+(require 'remember)
+(org-remember-insinuate)
+
+;; Start clock if a remember buffer includes :CLOCK-IN:
+(add-hook 'remember-mode-hook 'bh/start-clock-if-needed 'append)
+
+(defun bh/start-clock-if-needed ()
+  (save-excursion
+    (goto-char (point-min))
+    (when (re-search-forward " *:CLOCK-IN: *" nil t)
+      (replace-match "")
+      (org-clock-in))))
+
+;; I use C-M-r to start org-remember
+(global-set-key (kbd "C-M-r") 'org-remember)
+
+;; Keep clocks running
+(setq org-remember-clock-out-on-exit nil)
+
+;; C-c C-c stores the note immediately
+(setq org-remember-store-without-prompt t)
+
+;; I don't use this -- but set it in case I forget to specify a location in a future template
+(setq org-remember-default-headline "Tasks")
+
+;; 3 remember templates for TODO tasks, Notes, and Phone calls
+(setq org-remember-templates (quote (("todo" ?t "* TODO %?
+  %u
+  %a" nil bottom nil)
+                                     ("note" ?n "* %?                                        :NOTE:
+  %u
+  %a" nil bottom nil)
+                                     ("phone" ?p "* PHONE %:name - %:company -                :PHONE:
+  Contact Info: %a
+  %u
+  :CLOCK-IN:
+  %?" nil bottom nil))))
+(setq org-refile-targets (quote ((org-agenda-files :maxlevel . 5) (nil :maxlevel . 5))))
