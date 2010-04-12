126ce922defe3ba406b672bfc72c371f09bb27b8
Mike Fisher <mfisher@csh.rit.edu>
2010-04-12 14:15:36 -0400

    .emacs: clean up indentation, add sonnychiba sonnycorleone: clean up line ending sonnymcorleone: disable Chrome addon sonnychiba: initial checkin

---
+++ sonnychiba.el
@@ -0,0 +1,4 @@
+(autoload 'visual-basic-mode "visual-basic-mode" "Visual Basic mode." t)
+(setq auto-mode-alist (append '(("\\.\\(frm\\|bas\\|cls\\|vbs\\)$" .
+                                 visual-basic-mode)) auto-mode-alist))
+(setq visual-basic-mode-indent 4)
