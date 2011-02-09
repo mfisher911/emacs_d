aaca6b0deaa8d1864886f4ff82605a8bea02e882
Mike Fisher <mfisher@csh.rit.edu>
2011-02-08 23:03:50 -0500

    Updates to try to help network operations work more smoothly.
    Plan is to use gnus loaded by itself (emacs -q).

--- gnus.el
+++ gnus.el
@@ -1,11 +1,16 @@
-;; Stuff for GNUS
+;; Stuff for Gnus
 (require 'gnus)
 
-;; (setq gnus-check-new-newsgroups t)
-;; (setq gnus-save-killed-list t)
+(setq gnus-check-new-newsgroups 'ask-server)
+(setq gnus-save-killed-list t)
+(setq gnus-suppress-duplicates t)
+(setq gnus-save-duplicate-list t)
+(setq gnus-subscribe-newsgroup-method 'gnus-subscribe-interactively)
 (setq gnus-select-method
       '(nntp "news.csh.rit.edu"
              (nntp-open-connection-function nntp-open-ssl-stream)
-             (nntp-authinfo-file "~/.authinfo")
              (nntp-port-number "nntps") ;see /etc/services
              (nntp-address "news.csh.rit.edu")))
+
+;; redundant but helpful when this file is singly-loaded
+(menu-bar-mode -1)
