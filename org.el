e6eb91c4493ae646e34cb44eb945f1a740f1bd7f
Mike Fisher <Michael_Fisher@URMC.Rochester.edu>
2020-07-25 14:32:23 -0400

    Remove obsolete mysql-to-org-table

--- org.el
+++ org.el
@@ -167,19 +167,6 @@
 (add-to-list 'org-latex-packages-alist '("" "minted"))
 (setq org-latex-listings 'minted)
 
-;; de-mysql-table
-(defun mysql-to-org-table ()
-  "Convert a MySQL result table (bounded in +---+) to be an
-   Org table (bounded in |---|)."
-  (interactive)
-  (save-excursion
-    (goto-char (point-min))
-    (while (re-search-forward "^\\( +\\)\\+---" nil t)
-      (replace-match "\\1|---" nil nil))
-    (goto-char (point-min))
-    (while (re-search-forward "---\\+$" nil t)
-      (replace-match "---|" nil nil))))
-
 ;; Use MathJax from their CDN instead of the old version on the Org
 ;; servers.
 (setq org-html-mathjax-options
