e69a12d3f234776258d61d862ffe2f8bb3d2c12b
Mike Fisher <Michael_Fisher@URMC.Rochester.edu>
2020-07-25 14:30:19 -0400

    Remove unused my-org-convert-incoming-items

--- org.el
+++ org.el
@@ -136,39 +136,6 @@
 ;; 15.9 -- deadlines
 (setq org-deadline-warning-days 30)
 
-;; http://www.emacswiki.org/emacs-en/mobileorg (2010-01-27)
-(defun my-org-convert-incoming-items ()
-  (interactive)
-  (with-current-buffer (find-file-noselect org-mobile-inbox-for-pull)
-    (goto-char (point-min))
-    (while (re-search-forward "^\\* " nil t)
-      (goto-char (match-beginning 0))
-      (insert ?*)
-      (forward-char 2)
-      (insert "TODO ")
-      (goto-char (line-beginning-position))
-      (forward-line)
-      (insert
-       (format
-        " SCHEDULED: %s
-:PROPERTIES:
-:ID: %s :END:
-"
-        (with-temp-buffer (org-insert-time-stamp (current-time)))
-        (shell-command-to-string "uuidgen"))))
-    (let ((tasks (buffer-string)))
-      (erase-buffer)
-      (save-buffer)
-      (kill-buffer (current-buffer))
-      (with-current-buffer (find-file-noselect "~/Dropbox/todo.txt")
-        (save-excursion
-          (goto-char (point-min))
-          (search-forward "* CEG")
-          (goto-char (match-beginning 0))
-          (insert tasks))))))
- 
-;; (add-hook 'org-mobile-post-pull-hook 'my-org-convert-incoming-items)
-
 (setq org-mobile-use-encryption t)
 (setq org-export-latex-href-format "\\url{%s}")
 
