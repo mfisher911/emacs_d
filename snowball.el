311b980f65b905057ca571c69a9723c3ba8a2f66
Mike Fisher <mfisher@csh.rit.edu>
2020-11-28 11:39:24 -0500

    Remove flymake thumbs up/down status display

--- snowball.el
+++ snowball.el
@@ -23,23 +23,6 @@
 ;;   :config
 ;;   (diminish 'auto-fill-function (concat " " [#xF036])))
 
-;; Use thumbs-up / thumbs-down for flymake status.
-;; We need to reimplement `flymake-report-status' to make this happen.
-(eval-after-load 'flymake
-  '(defun flymake-report-status (e-w &optional status)
-     "Show status in mode line."
-     (when e-w
-       (setq flymake-mode-line-e-w e-w))
-     (when status
-       (setq flymake-mode-line-status status))
-     (let* ((mode-line " "))
-       (if (> (length flymake-mode-line-e-w) 0)
-           (setq mode-line (concat mode-line [#xF165] flymake-mode-line-e-w))
-         (setq mode-line (concat mode-line [#xF164])))
-       (setq mode-line (concat mode-line flymake-mode-line-status))
-       (setq flymake-mode-line mode-line)
-       (force-mode-line-update))))
-
 (use-package flycheck
   :ensure t
   :config
