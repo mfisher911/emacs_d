e2e4a8342c80b3b9c044ea1bc96cbf02bdc7df5d
Mike Fisher <mfisher911@gmail.com>
2021-03-21 17:58:24 -0400

    * Removed org-mode. * Added Magit. * Added Javascript2 mode. * Start the emacs-server.

--- lapblanket.el
+++ lapblanket.el
@@ -9,24 +9,24 @@
 (yas/initialize)
 (yas/load-directory "~/el/yasnippet-read-only/snippets")
 
-;;; org mode
-(load "~/.emacs.d/org.el" 'noerror)
-
 (require 'growl)
 (defun growl-appt-display (min-to-app new-time msg)
   (growl (format "Appointment in %s min." min-to-app)
          (format "Time: %s\n%s" new-time msg)))
 (setq appt-disp-window-function (function growl-appt-display))
 
-;; http://emacs-fu.blogspot.com/2009/11/showing-pop-ups.html
-(setq
-  appt-message-warning-time 15 ;; warn 15 min in advance
-  appt-display-mode-line t     ;; show in the modeline
-  appt-display-format 'window) ;; use our func
-(appt-activate 1)              ;; active appt (appointment notification)
-(display-time)                 ;; time display is required for this...
- ;; update appt each time agenda opened
+;; Chrome Edit Server -> Emacs
+;; (require 'edit-server)
+;; (edit-server-start)
+;; (setq edit-server-new-frame nil)
 
-(add-hook 'org-finalize-agenda-hook 'org-agenda-to-appt)
+;;; Magit
+(require 'magit)
+(global-set-key (kbd "C-x v \\") 'magit-status)
 
-;; (setq mac-command-modifier 'meta)
+;; Javascript2 mode.
+(autoload 'js2-mode "js2" nil t)
+(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+
+(server-mode 1)
+
