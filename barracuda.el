5c7cc166c50e6660ce4cc6f149d62a33f8dce2f7
Mike Fisher <mfisher911@gmail.com>
2021-03-21 17:59:36 -0400

    new config file

---
+++ barracuda.el
@@ -0,0 +1,98 @@
+;;; package -- Summary
+;;; Commentary:
+;;
+;; Computer-specific configuration items
+;;
+;;; Code:
+(setq user-full-name "Mike Fisher"
+      user-mail-address "mfisher911@gmail.com")
+
+;;; org mode
+(load "~/.emacs.d/org.el" 'noerror)
+
+(defalias 'list-buffers 'ibuffer)
+
+(setq calendar-location-name "Rochester, NY")
+(setq calendar-latitude 43.12)
+(setq calendar-longitude -77.63)
+
+(load-theme 'leuven)
+
+(use-package yaml-mode
+  :ensure t)
+
+(use-package flycheck
+  :ensure t
+  :init (global-flycheck-mode)
+  :config
+  (setq flycheck-display-errors-delay 3))
+
+(use-package pyvenv
+  :ensure t)
+
+(use-package elpy
+  :ensure t
+  :config
+  (elpy-enable)
+  (defalias 'workon 'pyvenv-workon))
+
+(use-package isortify
+  :ensure t
+  :config
+  (add-hook 'python-hook-mode 'isortify-mode))
+
+(use-package banner-comment
+  :ensure t
+  :commands (banner-comment)
+  :bind ("C-c h" . banner-comment))
+
+;; chmod +x
+;; ref. http://th.nao.ac.jp/MEMBER/zenitani/elisp-j.html#chmod
+(add-hook 'after-save-hook
+          'executable-make-buffer-file-executable-if-script-p)
+
+(use-package apache-mode
+  :ensure t)
+
+(use-package csv-mode
+  :ensure t)
+
+(use-package web-mode
+  :ensure t
+  :config
+  (setq web-mode-code-indent-offset 2))
+
+(use-package blacken
+  :ensure t
+  :config
+  (add-hook 'python-mode-hook 'blacken-mode)
+  (setq blacken-line-length 78))
+
+;; (mac-auto-operator-composition-mode)
+
+;; (use-package ligature
+;;   :load-path "~/el/ligature.el"
+;;   :ensure t
+;;   :config
+;;   ;; Enable the www ligature in every possible major mode
+;;   (ligature-set-ligatures 't '("www"))
+
+;;   ;; Enable ligatures in programming modes
+;;   (ligature-set-ligatures 'prog-mode '("www" "**" "***" "**/" "*>" "*/" "\\\\" "\\\\\\" "{-" "::"
+;;                                      ":::" ":=" "!!" "!=" "!==" "-}" "----" "-->" "->" "->>"
+;;                                      "-<" "-<<" "-~" "#{" "#[" "##" "###" "####" "#(" "#?" "#_"
+;;                                      "#_(" ".-" ".=" ".." "..<" "..." "?=" "??" ";;" "/*" "/**"
+;;                                      "/=" "/==" "/>" "//" "///" "&&" "||" "||=" "|=" "|>" "^=" "$>"
+;;                                      "++" "+++" "+>" "=:=" "==" "===" "==>" "=>" "=>>" "<="
+;;                                      "=<<" "=/=" ">-" ">=" ">=>" ">>" ">>-" ">>=" ">>>" "<*"
+;;                                      "<*>" "<|" "<|>" "<$" "<$>" "<!--" "<-" "<--" "<->" "<+"
+;;                                      "<+>" "<=" "<==" "<=>" "<=<" "<>" "<<" "<<-" "<<=" "<<<"
+;;                                      "<~" "<~~" "</" "</>" "~@" "~-" "~>" "~~" "~~>" "%%"))
+
+;;   ;; Enables ligature checks globally in all buffers. You can also do it
+;;   ;; per mode with `ligature-mode'.
+;;   (global-ligature-mode 't))
+
+
+(provide 'barracuda)
+;;; barracuda.el ends here
