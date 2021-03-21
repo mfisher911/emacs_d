fafac62aae9dd5ec7b323397d83e8272772b008d
Mike Fisher <mfisher911@gmail.com>
2021-03-21 17:58:23 -0400

    Changed some keybindings to help avoid pain & suffering at the hands of the flying Windows key as Meta.

--- sonnymcorleone.el
+++ sonnymcorleone.el
@@ -4,14 +4,20 @@
 (setq color-theme-is-global t)
 ;(color-theme-renegade)
 (color-theme-billw)
+(set-default-font "-apple-bitstream vera sans mono-medium-r-normal--12-120-72-72-m-120-iso10646-1")
 
 (autoload 'visual-basic-mode "visual-basic-mode" "Visual Basic mode." t)
 (setq auto-mode-alist (append '(("\\.\\(frm\\|bas\\|cls\\|vbs\\)$" .
                                  visual-basic-mode)) auto-mode-alist))
 (setq visual-basic-mode-indent 4)
+(setq mac-command-modifier 'meta)
+(setq x-select-enable-clipboard t)
+(setq mac-tool-bar-display-mode 'icons)
 
 ;; http://emacs-fu.blogspot.com/2009/04/dot-emacs-trickery.html
 ; Make scripts executable on save
 (add-hook 'after-save-hook 'executable-make-buffer-file-executable-if-script-p)
 ; Easy buffer switching by holding down shift and press any arrow key.
 (windmove-default-keybindings 'shift)
+
+(server-mode 1)
