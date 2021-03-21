e20b3638e2c4b2065108123909fc22a07cdb57ca
Mike Fisher <mfisher911@gmail.com>
2021-03-21 17:58:24 -0400

    Added growl interaction for org mode.

--- lapblanket.el
+++ lapblanket.el
@@ -7,7 +7,25 @@
 ;;; yasnippet
 (require 'yasnippet) ;; not yasnippet-bundle
 (yas/initialize)
-(yas/load-directory "~/.emacs.d/plugins/yasnippet-read-only/snippets")
+(yas/load-directory "~/el/yasnippet-read-only/snippets")
 
 ;;; org mode
 (load "~/.emacs.d/org.el" 'noerror)
+
+(require 'growl)
+(defun growl-appt-display (min-to-app new-time msg)
+  (growl (format "Appointment in %s min." min-to-app)
+         (format "Time: %s\n%s" new-time msg)))
+(setq appt-disp-window-function (function growl-appt-display))
+
+;; http://emacs-fu.blogspot.com/2009/11/showing-pop-ups.html
+(setq
+  appt-message-warning-time 12 ;; warn 15 min in advance
+  appt-display-mode-line t     ;; show in the modeline
+  appt-display-format 'window) ;; use our func
+(appt-activate 1)              ;; active appt (appointment notification)
+(display-time)                 ;; time display is required for this...
+ ;; update appt each time agenda opened
+
+(add-hook 'org-finalize-agenda-hook 'org-agenda-to-appt)
+
