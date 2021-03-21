2a36a091c3881ad60c4a3daa3bf55381cd70c308
Mike Fisher <mfisher911@gmail.com>
2021-03-21 17:58:24 -0400

    * Simplified tnt-loading. * Added handling to try to manage email signature file cleanup and   empty new-line handling in replies. * Added font-locking with u-vm-color.el for pico buffers. * Added Jabber handling.

--- edison.el
+++ edison.el
@@ -1,18 +1,8 @@
 ;; Stuff for TNT 
-(setq load-path (cons "/home/mfisher/tnt" load-path)) 
-(load "tnt") 
+;; (load "tnt") 
 (setq tnt-default-username "mfuXup") 
-;(setq tnt-buggy-idle nil) 
-(setq tnt-use-idle-timer nil) 
-(setq tnt-email-to-pipe-to nil) 
-(setq tnt-email-binary "/usr/bin/mail") 
 (setq tnt-use-timestamps t) 
-(setq tnt-beep-on-incoming-message 'audible) 
-(setq tnt-beep-on-visible-incoming-message 'audible) 
-(setq tnt-beep-on-chat-invitation 'audible) 
-(setq tnt-beep-on-chat-message 'audible) 
-(setq tnt-beep-on-visible-chat-message 'audible) 
-(set-face-foreground 'tnt-my-name-face "red") 
+; (set-face-foreground 'tnt-my-name-face "red") 
 
 (menu-bar-mode -1)
 (server-mode)
@@ -37,3 +27,63 @@
 (autoload 'twitter-status-edit "twitter" nil t)
 ;; (global-set-key "\C-xw" 'twitter-get-friends-timeline)
 (add-hook 'twitter-status-edit-mode-hook 'longlines-mode)
+
+;; Font lock for (Al)pine/pico buffers:
+;; http://snarfed.org/space/emacs%20font-lock%20faces%20for%20composing%20email
+(require 'u-vm-color)
+
+(defun maf-clear-msft-sig ()
+  "Clear out dumb Hotmail signatures."
+  ;;  (interactive)
+  (goto-char (point-min))
+  (while (re-search-forward "\\(^>\\s-*\\)\\{2,\\}$" nil t) 
+    (replace-match "> \n"))
+  (goto-char (point-min))
+  (while (re-search-forward "^\\(>>\\s-+\\)?>> --\\s-+\\(>> .*\\s-\\)+" nil t)
+    (replace-match ""))
+  (goto-char (point-min))
+  (while (re-search-forward "^>\\s-+> _+\\s-> .+\\s-> .+$" nil t)
+    (replace-match "\n"))
+  nil)
+
+; ugly heuristic to font-lock pine emails automatically
+(add-hook 'find-file-hooks
+          (lambda ()
+            (if (equal "pico." (substring (buffer-name (current-buffer)) 0 5))
+                (progn (maf-clear-msft-sig)
+                       (u-vm-color-fontify-buffer)))))
+
+;; these match pine's defaults. see M-x list-colors-display for other options
+(set-face-foreground 'u-vm-color-citation-1-face "cyan")
+(set-face-foreground 'u-vm-color-citation-2-face "lime green")
+(set-face-foreground 'u-vm-color-citation-3-face "blue")
+(set-face-foreground 'u-vm-color-citation-4-face "dark slate gray")
+(set-face-foreground 'u-vm-color-citation-5-face "dark slate blue")
+(set-face-foreground 'u-vm-color-signature-face  "red")
+
+;; Jabber.el -- has to be built from source 
+;; autoreconf -i
+;; ./configure && make
+;; sudo make install
+(require 'jabber-autoloads)
+(add-hook 'jabber-post-connect-hooks 'jabber-autoaway-start)
+(setq jabber-chat-header-line-format
+      '(" " (:eval (jabber-jid-displayname jabber-chatting-with))
+        " " (:eval (jabber-jid-resource jabber-chatting-with)) "\t";
+        (:eval (let ((buddy (jabber-jid-symbol jabber-chatting-with)))
+                 (propertize
+                  (or
+                   (cdr (assoc (get buddy 'show) jabber-presence-strings))
+                   (get buddy 'show))
+                  'face
+                  (or (cdr (assoc (get buddy 'show) jabber-presence-faces))
+                      'jabber-roster-user-online))))
+        "\t" (:eval (get (jabber-jid-symbol jabber-chatting-with) 'status))
+        (:eval (unless (equal "" *jabber-current-show*)
+                 (concat "\t You're " *jabber-current-show*
+                         " (" *jabber-current-status* ")")))))
+;; Message alert hooks
+(define-jabber-alert echo "Show a message in the echo area"
+  (lambda (msg)
+    (unless (minibuffer-prompt)
+      (message "%s" msg))))
