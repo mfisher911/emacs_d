46b710db1edb296f560c5a3cb15bf8fbb315dd4c
Mike Fisher <mfisher911@gmail.com>
2021-03-21 17:58:25 -0400

    Added org-checklist.

--- org.el
+++ org.el
@@ -3,6 +3,7 @@
 ;;; Org Mode
 ;;;
 (require 'org-install)
+(require 'org-checklist)
 (add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
 (global-set-key "\C-cl" 'org-store-link)
 (global-set-key "\C-ca" 'org-agenda)
