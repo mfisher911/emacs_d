29fe804f543eccf45ceeba23c13228e1590d3c10
Mike Fisher <mfisher911@gmail.com>
2021-03-21 17:58:24 -0400

    Added Magit.

--- sonhouse.el
+++ sonhouse.el
@@ -18,3 +18,25 @@
 (global-set-key (kbd "\C-c j a") 'jpl-reformat-align-enclosing-block)
 (global-set-key (kbd "\C-c j p") 'jpl-reformat-parameter-list-toggle-multiple-single)
 (require 'perlcritic)
+;; http://trey-jackson.blogspot.com/2008/01/emacs-tip-11-uniquify.html
+(require 'uniquify)
+(setq uniquify-buffer-name-style 'forward)
+(setq uniquify-separator "/")
+(setq uniquify-after-kill-buffer-p t) ; rename after killing uniquified
+
+;;; YASnippet
+(require 'yasnippet)
+(yas/initialize)
+(yas/load-directory "~/el/yasnippet-read-only/snippets")
+
+;;; ipython
+(require 'python-mode)
+(autoload 'python-mode "python-mode" "Python Mode." t)
+(add-to-list 'auto-mode-alist '("\\.py\\'" . python-mode))
+(add-to-list 'interpreter-mode-alist '("python" . python-mode))
+(require 'ipython)
+(setq ipython-completion-command-string
+      "print(';'.join(__IP.Completer.all_completions('%s')))\n")
+
+;;; Magit
+(require 'magit)
