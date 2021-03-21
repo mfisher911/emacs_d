ac3c136e809ce6b5899b1b12056f49ae876b491c
Mike Fisher <mfisher911@gmail.com>
2021-03-21 17:59:20 -0400

    Whitespace fix

--- init.el
+++ init.el
@@ -256,7 +256,7 @@ and subsequent lines as the task note."
   (jedi:setup))
 
 ;; python stuff from http://www.emacswiki.org/cgi-bin/wiki/PythonMode
-(add-hook 'python-mode-hook 
+(add-hook 'python-mode-hook
           '(lambda ()
              (define-key python-mode-map "\C-m" 'newline-and-indent)
              (setq show-trailing-whitespace t)))
