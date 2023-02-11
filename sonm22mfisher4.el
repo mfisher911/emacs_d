d69a1fda6563a38a938eaf64c7bf342c3f386b11
Mike Fisher <Michael_Fisher@URMC.Rochester.edu>
2023-02-11 15:41:10 -0500

    remove howardism.org alt-completing-read function

--- sonm22mfisher4.el
+++ sonm22mfisher4.el
@@ -588,39 +588,6 @@ killing them."
    org-agenda-mode-hook
    tabulated-list-mode-provide))
 
-;; http://www.howardism.org/Technical/Emacs/alt-completing-read.html
-(defun alt-completing-read (prompt collection &optional predicate require-match initial-input hist def inherit-input-method)
-  "Calls `completing-read' but returns the value from COLLECTION.
-
-Simple wrapper around the `completing-read' function that assumes
-the collection is either an alist, or a hash-table, and returns
-the _value_ of the choice, not the selected choice. For instance,
-give a variable of choices like:
-
-    (defvar favorite-hosts '((\"Glamdring\" . \"192.168.5.12\")
-                             (\"Orcrist\"   . \"192.168.5.10\")
-                             (\"Sting\"     . \"192.168.5.220\")
-                             (\"Gungnir\"   . \"192.168.5.25\")))
-
-We can use this function to `interactive' without needing to call
-`alist-get' afterwards:
-
-    (defun favorite-ssh (hostname)
-      \"Start a SSH session to a given HOSTNAME.\"
-      (interactive (list (alt-completing-read \"Host: \" favorite-hosts)))
-      (message \"Rockin' and rollin' to %s\" hostname))"
-
-  ;; Yes, Emacs really should have an `alistp' predicate to make this code more readable:
-  (cl-flet ((assoc-list-p (obj) (and (listp obj) (consp (car obj)))))
-
-    (let* ((choice
-            (completing-read prompt collection predicate require-match initial-input hist def inherit-input-method))
-           (results (cond
-                     ((hash-table-p collection) (gethash choice collection))
-                     ((assoc-list-p collection) (alist-get choice collection def nil 'equal))
-                     (t                         choice))))
-      (if (listp results) (first results) results))))
-
 ;; 2023-02-03
 (use-package powershell)
 
